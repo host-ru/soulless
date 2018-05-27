9bef97cbd83fddce8f3d5e4d0090289d1f78be0f
host-ru <andrey1gorbulin@gmail.com>
2018-05-27 15:37:25 +0300

    Refactored terrain

---
+++ Soulless/Assets/Scripts/ProceduralGeneration/Data/MeshSettings.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class MeshSettings : UpdatableData {
+
+    public float meshScale = 2.5f;
+    public bool useFlatShading;
+
+    public const int numSupportedLODs = 5;
+    public const int numSupportedChunkSizes = 9;
+    public const int numSupportedFlatshadedChunkSizes = 3;
+    public static readonly int[] supportedChunkSizes = { 48, 72, 96, 120, 144, 168, 192, 216, 240 };
+
+    [Range(0, numSupportedChunkSizes - 1)]
+    public int chunkSizeIndex;
+    [Range(0, numSupportedFlatshadedChunkSizes - 1)]
+    public int flatshadedChunkSizeIndex;
+
+    // Num verts per line of mes rendered at LOD0. Includes the 2 extra vertices that are excluded from final mesh, but used for calculating normals
+    public int numVertsPerLine
+    {
+        get
+        {
+            return supportedChunkSizes[(useFlatShading) ? flatshadedChunkSizeIndex : chunkSizeIndex] + 1; 
+        }
+    }
+
+    public float meshWorldSize
+    {
+        get
+        {
+            return(numVertsPerLine - 3) * meshScale;
+        }
+    }
+}
